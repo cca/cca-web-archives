8c96620a757fab9017ec6ae47bc89352b064f19f
phette23 <phette23@gmail.com>
2026-04-03 14:52:57 -0700

    feat: print out session account info

--- ia/main.py
+++ ia/main.py
@@ -1,5 +1,6 @@
 import sys
 from os import environ
+from typing import Any
 
 from dotenv import load_dotenv
 from internetarchive import Item, configure, get_session
@@ -19,7 +20,14 @@ def main(identifiers: list[str]) -> None:
         config_file="ia.ini",
     )
     session = get_session(config_file="ia.ini")
-    print(session.whoami())
+    if session:
+        # type hint for this method is wrong, it returns a dict not a string
+        whoami: dict[str, Any] = session.whoami()  # type: ignore
+        user_info = whoami.get("value", {})
+        print(
+            f"Logged in as {user_info.get('itemname')} {user_info.get('username')} {user_info.get('screenname')}"
+        )
+
     for identifier in identifiers:
         item: Item = session.get_item(identifier)
         print(item.metadata)
